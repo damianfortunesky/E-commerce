3879574e5e99b01120490eba755acf9c412fb342
damian.fortunesky <damianfortunesky@gmail.com>
2022-06-17 15:40:57 -0300

    Creo funciones para agregar cursos y leer datos de los mismos

--- js/app.js
+++ js/app.js
@@ -19,3 +19,49 @@ function cargarEventListeners() {
      vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
 
 }
+
+// Funciones
+
+// Función que añade el curso al carrito
+function agregarCurso(e) { // esta
+     e.preventDefault();
+     // Delegation para agregar-carrito
+     if(e.target.classList.contains('agregar-carrito')) {
+          const curso = e.target.parentElement.parentElement;
+          // Enviamos el curso seleccionado para tomar sus datos
+          leerDatosCurso(curso);
+     }
+}
+
+// Lee los datos del curso
+function leerDatosCurso(curso) { // esta
+     const infoCurso = {
+          imagen: curso.querySelector('img').src,
+          titulo: curso.querySelector('h4').textContent,
+          precio: curso.querySelector('.precio span').textContent,
+          id: curso.querySelector('a').getAttribute('data-id'), 
+          cantidad: 1
+     }
+
+
+     if( articulosCarrito.some( curso => curso.id === infoCurso.id ) ) { 
+          const cursos = articulosCarrito.map( curso => {
+               if( curso.id === infoCurso.id ) {
+                    curso.cantidad++;
+                     return curso;
+                } else {
+                     return curso;
+             }
+          })
+          articulosCarrito = [...cursos];
+     }  else {
+          articulosCarrito = [...articulosCarrito, infoCurso];
+     }
+
+     // console.log(articulosCarrito)
+
+     
+
+     // console.log(articulosCarrito)
+     carritoHTML();
+}
